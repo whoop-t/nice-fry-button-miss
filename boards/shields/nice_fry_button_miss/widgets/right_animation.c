5ada2a305b291afe756c25487e8a0da7d194a520
whoop-t <tommymallis@gmail.com>
2025-03-07 22:37:03 -0800

    add bonk finger iamge

--- boards/shields/nice_fry_button_miss/widgets/right_animation.c
+++ boards/shields/nice_fry_button_miss/widgets/right_animation.c
@@ -5,9 +5,11 @@
 LV_IMG_DECLARE(right_image);
 LV_IMG_DECLARE(right_image_2);
 LV_IMG_DECLARE(right_image_3);
+LV_IMG_DECLARE(right_image_4);
+LV_IMG_DECLARE(right_image_5);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &right_image, &right_image_2, &right_image_3,
+    &right_image, &right_image_2, &right_image_3, &right_image_4, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5, &right_image_5
 };
 
 void draw_right_animation(lv_obj_t *canvas) {
@@ -15,7 +17,7 @@ void draw_right_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 3);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 16);
     lv_animimg_set_duration(art, CONFIG_NICE_RIGHT_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
